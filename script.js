dd5fd4ce5ebeebb915300576be33adfeba2910f1
martendo <51766598+martendo@users.noreply.github.com>
2023-11-17 17:54:54 +0000

    Add floating content

--- script.js
+++ script.js
@@ -14,7 +14,21 @@ buttonsoon.addEventListener("click", () => {
 	buttonsoon.innerHTML = `Sign Up<br>in ${soontime} seconds`;
 });
 
+const cookies = document.getElementById("popupcookies");
 document.getElementById("buttoncookiesok").addEventListener("click", () => {
-	document.getElementById("popupcookies").style.display = "none";
+	cookies.style.display = "none";
 	signup.style.display = "grid";
+});
+
+const float = document.getElementById("floatad");
+let floatleft = false;
+document.getElementById("buttonfloatclose").addEventListener("click", () => {
+	floatleft = !floatleft;
+	if (floatleft) {
+		float.style.right = "unset";
+		float.style.left = "20px";
+	} else {
+		float.style.right = "";
+		float.style.left = "";
+	}
 });
